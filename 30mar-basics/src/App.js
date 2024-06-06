d6079f5f03d21858ecfc29922cd48d1987997b52
harshita <harshita@logicrays.com>
2024-06-06 08:49:01 +0530

    06junfirstcommit

---
+++ 30mar-basics/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+         <h1 className='App text-danger'>Hello React</h1>
+        <h2>Welcome to LRA</h2>
+    </div>
+  );     
+}
+
+export default App;
